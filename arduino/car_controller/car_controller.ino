af7e200d8f9ca4a1ba6da4e4388fc51b8771915b
Thomas <thomas32426@gmail.com>
2016-10-14 12:06:20 -0400

    Yeeeeeeaaaaaaah. <-- That's terrible. Come on, don't do that.

--- arduino/car_controller/car_controller.ino
+++ arduino/car_controller/car_controller.ino
@@ -5,12 +5,12 @@
 
 #include <ros.h>
 #include <std_msgs/UInt16.h>
+#include <std_msgs/Bool.h>
 
 const int LED_PIN = 13;
-const int STEERING_PIN = 2;
-const int THROTTLE_PIN = 3;
-const int MODE_PIN = 4;
-const int RECORDING_PIN = 5;
+const int STEERING_PIN = 2; // 670 - 1240
+const int THROTTLE_PIN = 3; //570 - 1330
+//const int MODE_PIN = 4;
 const int LEFT_MOTOR_PIN = 9;
 const int RIGHT_MOTOR_PIN = 10;
 
@@ -21,33 +21,58 @@ const int DEAD_BAND = 100;
 const int STATIC_OFFSET = 10;
 const int RX_PWM_ERROR = 400;
 const int SPEED_LIMIT = 100;
+const int ARMED = 500; // 462
 
 unsigned int SteeringPulse;
 unsigned int ThrottlePulse;
-unsigned int ModePulse;
-unsigned int RecordingPulse;
+//unsigned int ModePulse;
 unsigned int ThrottleRight;
 unsigned int ThrottleLeft;
 unsigned int Factor;
 boolean Autonomous;
-boolean GTG = true;
-
+boolean full = false;
+boolean rec = false;
 
 ros::NodeHandle nh;
 std_msgs::UInt16 PulseMsg;
 
 ros::Publisher SteeringPub("steering_pwm", &PulseMsg);
 ros::Publisher ThrottlePub("throttle_pwm", &PulseMsg);
-ros::Publisher ModePub("mode_pwm", &PulseMsg);
-ros::Publisher RecordingStatePub("recording_state", &PulseMsg);
+//ros::Publisher ModePub("mode_pwm", &PulseMsg);
+ros::Subscriber<std_msgs::Bool> FullSub("full_msg", &fullCb);
+ros::Subscriber<std_msgs::Bool> RecSub("rec_msg", &recCb);
 
 void StartRos()
 {
   nh.initNode();
   nh.advertise(SteeringPub);
   nh.advertise(ThrottlePub);
-  nh.advertise(ModePub);
-  nh.advertise(RecordingStatePub);
+  //nh.advertise(ModePub);
+  nh.subscribe(FullSub)
+}
+
+void fullCb(const std_msgs::Bool& full_msg)
+{
+  if(full_msg==true && full==false)
+  {
+    full = true;
+  }
+  else if(full_msg==false && full==true)
+  {
+    full = false;
+  }
+}
+
+void recCb(const std_msgs::Bool& rec_msg)
+{
+  if(rec_msg==true && rec==false)
+  {
+    rec = true;
+  }
+  else if(rec_msg==false && rec==true)
+  {
+    rec = false;
+  }
 }
 
 void PubPulse(const int &_channel, const uint16_t &_pulse)
@@ -62,14 +87,12 @@ void PubPulse(const int &_channel, const uint16_t &_pulse)
   {
     ThrottlePub.publish( &PulseMsg);
   }
+  /*
   else if (_channel == MODE_PIN)
   {
     ModePub.publish( &PulseMsg);
   }
-  else if (_channel == RECORDING_PIN)
-  {
-    RecordingStatePub.publish( &PulseMsg);
-  }
+  */
 }
 
 void GetInput()
@@ -80,19 +103,25 @@ void GetInput()
   ThrottlePulse = pulseIn(THROTTLE_PIN , HIGH, 20000) - RX_PWM_ERROR;
   PubPulse(THROTTLE_PIN, ThrottlePulse);
   
+  /*
   ModePulse = pulseIn(MODE_PIN , HIGH, 20000) - RX_PWM_ERROR;
   PubPulse(MODE_PIN, ModePulse);
-  
-  RecordingPulse = pulseIn(RECORDING_PIN , HIGH, 20000) - RX_PWM_ERROR;
-  PubPulse(RECORDING_PIN, RecordingPulse);
+  */
 }
 
 void Output()
 {
   //activation controlled by mode channel
-  if (abs(ModePulse - MID_PWM) < 100) //ModePulse ~= mid pwm
+  if (ThrottlePulse > ARMED) // Check if armed/recording
   {
-    digitalWrite(LED_PIN, HIGH);
+    if(!full)
+    {
+      if(rec)
+      {
+        digitalWrite(LED_PIN, HIGH); // turn on light
+      }
+      else
+    }
 
     //throttle
      if(ThrottlePulse > 1500 + DEAD_BAND) 
@@ -143,10 +172,6 @@ void Output()
   {
     digitalWrite(LEFT_MOTOR_PIN, LOW);
     digitalWrite(RIGHT_MOTOR_PIN, LOW); 
-    digitalWrite(LED_PIN, HIGH); // Blinking LED = switch in off mode
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(100);
   }
 }
 
@@ -155,7 +180,6 @@ void setup()
   pinMode(STEERING_PIN, INPUT);
   pinMode(THROTTLE_PIN, INPUT);
   pinMode(MODE_PIN, INPUT);
-  pinMode(RECORDING_PIN, INPUT); 
   pinMode(LED_PIN, OUTPUT);
   pinMode(LEFT_MOTOR_PIN, OUTPUT);
   pinMode(RIGHT_MOTOR_PIN, OUTPUT);
