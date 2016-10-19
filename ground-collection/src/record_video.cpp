33b4cd4985fd6823b8c1bffbec932d33801a2db7
thomas32426 <thomas32426@gmail.com>
2016-10-19 03:19:02 +0000

    Noooooooooooooooooooooooooooooooooo. <-- Don't do that. That's just silly.

--- ground-collection/src/record_video.cpp
+++ ground-collection/src/record_video.cpp
@@ -2,19 +2,52 @@
 
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
+#include "std_msgs/UInt16.h"
+#include <rosbag/bag.h>
+#include <string>
+#include <sensor_msgs/Image.h>
+
+//using namespace std;
+
+rosbag::Bag bag;
+std_msgs::UInt16 steering_msg;
+std_msgs::Bool armed_msg;
+bool newBag = true;
+
+void steering_callback(const std_msgs::UInt16::ConstPtr& msg)
+{
+	steering_msg.data = msg->data;
+}
+
+void armed_callback(const std_msgs::Bool::ConstPtr& msg)
+{
+	armed_msg.data = msg->data;
+}
+
+void image_callback(const sensor_msgs::Image::ConstPtr& msg)
+{
+	if(/*armed_msg.data &&*/ !newBag)
+    {
+        bag.write("usb_cam/image_raw",ros::Time::now(), msg);
+        bag.write("steering_pwm",ros::Time::now(), steering_msg);
+    }
+}
 
 int main(int argc, char **argv)
 {
+	steering_msg.data = 0;
 
 	ros::init(argc, argv, "record_video");
-
 	ros::NodeHandle nh;
+	std::string bag_filename = std::to_string(ros::Time::now().toSec());
 
 	ros::Publisher Full_Msg = nh.advertise<std_msgs::Bool>("full_msg", 1000);
 	ros::Publisher Rec_Msg = nh.advertise<std_msgs::Bool>("rec_msg",1000);
-	ros::Rate loop_rate(10);
+	ros::Subscriber steering_sub = nh.subscribe("steering_pwm", 1, steering_callback);
+	ros::Subscriber armed_sub = nh.subscribe("armed", 1, armed_callback);	
+	ros::Subscriber camera_sub = nh.subscribe("usb_cam/image_raw", 1, image_callback);
 
-	int counter = 0;	
+	ros::Rate loop_rate(10);
 
 	while(ros::ok())
 	{
@@ -26,9 +59,21 @@ int main(int argc, char **argv)
 
 		Full_Msg.publish(full_msg);
 		Rec_Msg.publish(rec_msg);
-
+		
+		if(/*armed_msg.data &&*/ newBag)
+		{
+			bag_filename = std::to_string(ros::Time::now().toSec());
+			bag.open(bag_filename, rosbag::bagmode::Write);
+			newBag = false;
+		}
+		else if(/*!armed_msg.data &&*/ !newBag)
+		{
+			//bag.close();
+			//newBag = true;
+		}
+		
 		ros::spinOnce();
 	}	
-
+	bag.close();
 	return 0;
 }
