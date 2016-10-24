d42565c564ec258b20cac9cffaf615452d84759a
shadySource <alec0061@hotmail.com>
2016-10-24 18:30:38 -0400

    configure h bridge based control

--- arduino/car_controller/car_controller.ino
+++ arduino/car_controller/car_controller.ino
@@ -9,27 +9,33 @@
 #include <ros.h>
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Bool.h>
+#include <servo.h>
 
-const int LED_PIN = 13;
+const int LED_PIN = 6;
 const int STEERING_PIN = 2; // 670 - 1240
 const int THROTTLE_PIN = 3; //570 - 1330
-const int LEFT_MOTOR_PIN = 9;
-const int RIGHT_MOTOR_PIN = 10;
+const int FORWARD_MOTOR_PIN = 9;
+const int REVERSE_MOTOR_PIN = 10;
+const int MOTOR_PIN = 11;
+const int SERVO_PIN = 15;
 
 const int HIGH_PWM = 2000;
 const int MID_PWM = 1500;
 const int LOW_PWM = 1000;
 const int DEAD_BAND = 100;
 const int STATIC_OFFSET = 10;
-const int RX_PWM_ERROR = 400;
+const int RX_PWM_ERROR = 0;
 const int SPEED_LIMIT = 100;
 const int ARMED = 1000; // 462
+
+Servo Servo1;
 
 unsigned int SteeringPulse;
 unsigned int ThrottlePulse;
 unsigned int ThrottleRight;
 unsigned int ThrottleLeft;
 unsigned int Factor;
+bool Reverse;
 boolean Autonomous;
 boolean Full = false;
 boolean Rec = false;
@@ -54,13 +60,38 @@ void GetInput()
 
   ThrottlePulse = pulseIn(THROTTLE_PIN , HIGH, 20000) - RX_PWM_ERROR;
 
-  if (ThrottlePulse > 1000)
+  if (abs(ThrottlePulse - 1500) > DEAD_BAND)
   {
     ArmedMsg.data = true;
   }
-  else
+  else 
   {
     ArmedMsg.data = false;
+  }
+}
+
+void setDirection(bool forward) // 1 for forward, 0 for reverse
+{
+  ///set direction using logic pins of hbridge
+  if (forward == Forward)
+  {
+    // do nothing
+  }
+  else
+  {
+    // change directions
+    if (Forward == true)
+    {
+      digitalWrite(FORWARD_MOTOR_PIN, LOW);
+      digitalWrite(REVERSE_MOTOR_PIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(REVERSE_MOTOR_PIN, LOW);
+      digitalWrite(FORWARD_MOTOR_PIN, HIGH);
+    }
+    //store current state
+    Forward = forward;
   }
 }
 
@@ -69,58 +100,36 @@ void Output()
   /// Publish the ROS messages:
   SteeringPub.publish( &PulseMsg );
   ArmedPub.publish( &ArmedMsg );
-  if (ArmedMsg.data)
+
+  if (ArmedMsg.data) // ArmedMsg.data is assigned in GetInputs()
   {
-    //throttle
-    if(ThrottlePulse > 1500 + DEAD_BAND) 
+    //throttle magnitide
+    //map(in, in_min, in_max, out_min, out_max)
+    ThrottlePulse = map(abs(1500-ThrottlePulse, 0, 500, 0, 255);
+
+    //throttle direction
+    if(ThrottlePulse > 1500) //Deadband considered in ArmedMsg.data
     {
-        ThrottlePulse = map(ThrottlePulse, 1500 + DEAD_BAND, 1880, 0, SPEED_LIMIT);
+      setDirection(1); // 1 for forward
     }
-    else if(ThrottlePulse < 1480-DEAD_BAND) 
-    {
-        ThrottlePulse = map(ThrottlePulse, 1500 - DEAD_BAND, 1080, 0, -1*SPEED_LIMIT);
-    } 
     else 
     {
-        ThrottlePulse = 0;
+      setDirection(0); // 0 for reverse
     }
-      
-    //steer
-    if(SteeringPulse > 1500 + DEAD_BAND) // Turn left
-    {
-      Factor = map(SteeringPulse, 1500 + DEAD_BAND, 1880, 0, 100);
-      // Right motor  CW (tune with 80)
-      ThrottleRight = MID_PWM + ThrottlePulse + 1*ThrottlePulse*Factor/100;
-      // Left  motor CCW
-      ThrottleLeft  = MID_PWM - ThrottlePulse + 1*ThrottlePulse*Factor/100;
-      }
-      else if(SteeringPulse < 1500 - DEAD_BAND) // Turn right 
-      {
-        Factor = map(SteeringPulse, 1500 - DEAD_BAND, 1080, 0, 100);
-        ThrottleLeft  = MID_PWM - ThrottlePulse - 1*ThrottlePulse*Factor/100;
-        ThrottleRight = MID_PWM + ThrottlePulse - 1*ThrottlePulse*Factor/100;
-      }
-      else
-      {
-        ThrottleLeft  = MID_PWM - ThrottlePulse;
-        ThrottleRight = MID_PWM + ThrottlePulse + STATIC_OFFSET;
-      }
-      
-      //control motors
-      digitalWrite(RIGHT_MOTOR_PIN, HIGH);
-      delayMicroseconds(ThrottleRight);
-      digitalWrite(RIGHT_MOTOR_PIN, LOW);
-      delayMicroseconds(20000 - ThrottleRight);
-      digitalWrite(LEFT_MOTOR_PIN, HIGH);
-      delayMicroseconds(ThrottleLeft);
-      digitalWrite(LEFT_MOTOR_PIN, LOW);
-      delayMicroseconds(20000 - ThrottleLeft);
+    
+    //steering servo direction
+    SteeringPulse = map(SteeringPulse, 1000, 2000, 30, 150);
+
+    //control servo
+    Servo1.write(SteeringPulse);
+
+    //control motors
+    analogWrite(MOTOR_PIN, ThrottlePulse);
   } 
   else //ModePulse is not at mid
   {
-    digitalWrite(LEFT_MOTOR_PIN, LOW);
+    digitalWrite(MOTOR_PIN, LOW);
     delay(100);
-    digitalWrite(RIGHT_MOTOR_PIN, LOW); 
   }
 }
 
@@ -129,9 +138,13 @@ void setup()
   pinMode(STEERING_PIN, INPUT);
   pinMode(THROTTLE_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(LEFT_MOTOR_PIN, OUTPUT);
-  pinMode(RIGHT_MOTOR_PIN, OUTPUT);
+  pinMode(FORWARD_MOTOR_PIN, OUTPUT);
+  pinMode(REVERSE_MOTOR_PIN, OUTPUT);
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
   
+  Servo1.attach(SERVO_PIN);
+
   StartRos();
 }
 
