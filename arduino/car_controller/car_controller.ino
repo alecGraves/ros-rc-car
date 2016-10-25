61933a28c0fdf24b7a3abd96e2e0be23480a21b3
shadySource <alec0061@hotmail.com>
2016-10-24 23:01:22 -0400

    add mode pwm input

--- arduino/car_controller/car_controller.ino
+++ arduino/car_controller/car_controller.ino
@@ -18,7 +18,7 @@ const int FORWARD_MOTOR_PIN = 9;
 const int REVERSE_MOTOR_PIN = 10;
 const int MOTOR_PIN = 11;
 const int SERVO_PIN = 15;
-
+const int MODE_PIN = 10;
 const int HIGH_PWM = 2000;
 const int MID_PWM = 1500;
 const int LOW_PWM = 1000;
@@ -26,7 +26,6 @@ const int DEAD_BAND = 100;
 const int STATIC_OFFSET = 10;
 const int RX_PWM_ERROR = -150;
 const int SPEED_LIMIT = 100;
-const int ARMED = 1000; // 462
 
 const boolean DoDebug = true;
 const boolean ReverseThrottle = true;
@@ -34,19 +33,22 @@ Servo Servo1;
 
 unsigned int SteeringPulse;
 unsigned int ThrottlePulse;
+unsigned int ModePulse;
 unsigned int ThrottleRight;
 unsigned int ThrottleLeft;
 unsigned int Factor;
 boolean Forward;
 
-boolean Autonomous;
+enum Mode { disarmed, armed, autonomous };
+
+Mode CurrentMode;
 
 
 ros::NodeHandle nh;
 std_msgs::UInt16 PulseMsg;
-std_msgs::Bool ArmedMsg;
+std_msgs::Bool MovingMsg;
 
-ros::Publisher ArmedPub("armed", &ArmedMsg);
+ros::Publisher MovingPub("moving", &MovingMsg);
 ros::Publisher SteeringPub("steering_pwm", &PulseMsg);
 ros::Publisher ThrottlePub("throttle_pwm", &PulseMsg);
 
@@ -54,7 +56,7 @@ ros::Publisher ThrottlePub("throttle_pwm", &PulseMsg);
 void StartRos()
 {
   nh.initNode();
-  nh.advertise(ArmedPub);
+  nh.advertise(MovingPub);
   nh.advertise(SteeringPub);
   nh.advertise(ThrottlePub);
 }
@@ -71,6 +73,8 @@ void GetInput()
   {
     ThrottlePulse = pulseIn(THROTTLE_PIN , HIGH, 20000) - RX_PWM_ERROR;
   }
+
+  ModePulse = pulseIn(MODE_PIN , HIGH, 20000) - RX_PWM_ERROR;
 }
 
 void setDirection(bool forward) // 1 for forward, 0 for reverse
@@ -100,42 +104,58 @@ void setDirection(bool forward) // 1 for forward, 0 for reverse
 
 void Output()
 {
-  // Armed?
+  // moving?
   if (abs(ThrottlePulse - 1500) > DEAD_BAND)
   {
-    ArmedMsg.data = true;
+    MovingMsg.data = true;
   }
   else 
   {
-    ArmedMsg.data = false;
+    MovingMsg.data = false;
+  }
+
+  //mode?
+  if (ModePulse < 1400)
+  {
+    CurrentMode = disarmed;
+  }
+  else
+  {
+    CurrentMode = armed;
   }
 
   /// Publish the ROS messages:
   PulseMsg.data = SteeringPulse;
   SteeringPub.publish(&PulseMsg);
+  MovingPub.publish(&MovingMsg);
   if (DoDebug)
   {
     PulseMsg.data = ThrottlePulse;
     ThrottlePub.publish(&PulseMsg);
-  }
-  ArmedPub.publish(&ArmedMsg);
-  
+  }  
 
-  if (ArmedMsg.data) // ArmedMsg.data is assigned above
+  if (CurrentMode == armed)
   {
     digitalWrite(LED_PIN, HIGH);
-    //throttle magnitide
-    //map(in, in_min, in_max, out_min, out_max)
-    ThrottlePulse = map( abs(1500-ThrottlePulse), 0, 500, 0, 255);
 
-    //throttle direction
-    if(ThrottlePulse > 1500) //Deadband considered in ArmedMsg.data
+    if (MovingMsg.data)
     {
-      setDirection(1); // 1 for forward
-    }
-    else 
-    {
-      setDirection(0); // 0 for reverse
+      //throttle magnitide
+      //map(in, in_min, in_max, out_min, out_max)
+      ThrottlePulse = map( abs(1500-ThrottlePulse), 0, 500, 0, 255);
+
+      //throttle direction
+      if(ThrottlePulse > 1500) //Deadband considered in MovingMsg.data
+      {
+        setDirection(1); // 1 for forward
+      }
+      else 
+      {
+        setDirection(0); // 0 for reverse
+      }
+
+      //control motors
+      analogWrite(MOTOR_PIN, ThrottlePulse);
     }
     
     //steering servo direction
@@ -144,18 +164,16 @@ void Output()
     //control servo
     Servo1.write(SteeringPulse);
 
-    //control motors
-    analogWrite(MOTOR_PIN, ThrottlePulse);
   } 
-  else //ModePulse is not at mid
+  else //disarmed
   {
+    //disable motor and blink
     digitalWrite(LED_PIN, LOW);
     digitalWrite(MOTOR_PIN, LOW);
     delay(100);
     digitalWrite(LED_PIN, HIGH);
     delay(100);
     digitalWrite(LED_PIN, LOW);
-
   }
 }
 
@@ -163,6 +181,7 @@ void setup()
 {
   pinMode(STEERING_PIN, INPUT);
   pinMode(THROTTLE_PIN, INPUT);
+  pinMode(MODE_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(FORWARD_MOTOR_PIN, OUTPUT);
   pinMode(REVERSE_MOTOR_PIN, OUTPUT);
