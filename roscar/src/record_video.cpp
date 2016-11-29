0a063fe2c4db8994b60e5661b02b3d74d5f89205
thomas32426 <thomas32426@gmail.com>
2016-11-29 00:49:59 +0000

    add opencv for image downscaling and manipulation

--- roscar/src/record_video.cpp
+++ roscar/src/record_video.cpp
@@ -1,16 +1,42 @@
-#include "ros/ros.h"
-#include "std_msgs/Bool.h"
-#include "std_msgs/UInt16.h"
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
 #include <rosbag/bag.h>
-#include <string>
 #include <sensor_msgs/Image.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/UInt16.h>
+#include <string>
 
 using namespace std;
 
 rosbag::Bag bag;
 std_msgs::UInt16 steering_msg;
 std_msgs::Bool moving;
+sensor_msgs::ImageConstPtr camera_msg;
+ros::Publisher image_pub;
 bool newBag = true;
+
+void downscale_image(const sensor_msgs::Image::ConstPtr& original_image)
+{
+	//Convert from the ROS image message to a CvImage suitable for working with OpenCV for processing
+	cv_bridge::CvImagePtr cv_ptr;
+	try
+	{
+		//Always copy, returning a mutable CvImage
+		//OpenCV expects color images to use BGR channel order.
+		cv_ptr = cv_bridge::toCvCopy(original_image, sensor_msgs::image_encodings::BGR8);
+	}
+	catch (cv_bridge::Exception& e)
+	{
+		//if there is an error during conversion, display it
+		ROS_ERROR("color_tracker.cpp::cv_bridge exception: %s", e.what());
+		return;
+	}
+	sensor_msgs::ImagePtr thresh;
+	//downscale resolution by 1/5:
+	cv::resize(cv_ptr->image, cv_ptr->image, cv::Size(), 0.1, 0.1);
+	cv::flip(cv_ptr->image, cv_ptr->image, -1);
+	camera_msg = cv_ptr->toImageMsg();
+}
 
 void steering_callback(const std_msgs::UInt16::ConstPtr& msg)
 {
@@ -24,9 +50,11 @@ void armed_callback(const std_msgs::Bool::ConstPtr& msg)
 
 void image_callback(const sensor_msgs::Image::ConstPtr& msg)
 {
+	downscale_image(msg);
+	image_pub.publish(camera_msg);
 	if(moving.data && !newBag)
     {
-        bag.write("usb_cam/image_raw",ros::Time::now(), msg);
+        bag.write("usb_cam/image_raw",ros::Time::now(), camera_msg);
         bag.write("steering_pwm",ros::Time::now(), steering_msg);
     }
 }
@@ -41,6 +69,7 @@ int main(int argc, char **argv)
 	double creation_time = ros::Time::now().toSec();
 	double time_diff = 3;
 
+	image_pub = nh.advertise<sensor_msgs::Image>("stored_image", 1);
 	ros::Subscriber steering_sub = nh.subscribe("steering_pwm", 1, steering_callback);
 	ros::Subscriber armed_sub = nh.subscribe("moving", 1, armed_callback);	
 	ros::Subscriber camera_sub = nh.subscribe("usb_cam/image_raw", 1, image_callback);
